b816662f7085a5c48d19260abed2975170cdb406
Alexander Drozdov <adrozdoff@gmail.com>
2015-01-18 02:07:02 +1000

    Make CKosFile non-copyable but movable.

--- linux/kosFile.h
+++ linux/kosFile.h
@@ -12,6 +12,11 @@ public:
 	int Seek(int seekFrom, int seekStep);
 	void LoadTex(Byte *targetPtr, Byte Size, int width, int height);
 
+    CKosFile(const CKosFile&) = delete;
+    CKosFile& operator=(const CKosFile&) = delete;
+    CKosFile(CKosFile&&) = default;
+    CKosFile& operator=(CKosFile&&) = default;
+
 private:
     FILE *m_fp = nullptr;
 };
