92be1e508dd4288324a502f8b5ed32dbd3cf80d0
Alexander Drozdov <adrozdoff@gmail.com>
2015-01-18 02:10:48 +1000

    Make CKosRender non-copyable

--- render.h
+++ render.h
@@ -12,4 +12,14 @@ public:
 	void RenderImg(RGB *img, Point position, int width, int height);
 	int getPixel(int x, int y);
 //	void DrawImage(CKosImage *img, int x, int y, int angle);
+
+#if __cplusplus >= 201103L
+    CKosRender(const CKosRender&) = delete;
+    CKosRender& operator=(const CKosRender&) = delete;
+    CKosRender(CKosRender&&) = default;
+    CKosRender& operator=(CKosRender&&) = default;
+#else
+    CKosRender(const CKosRender&);
+    CKosRender& operator=(const CKosRender&);
+#endif
 };
